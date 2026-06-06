df6182cdf3a873e37b9d48a76e6188e35f463720
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-06-06 17:13:37 +0530

    Variation Manager for Variation Model

--- carts/views.py
+++ carts/views.py
@@ -14,9 +14,11 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    # size = request.GET['size']
-    return HttpResponse(color)
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        print(color, size)
+
     product = Product.objects.get(id=product_id) # get the product
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
