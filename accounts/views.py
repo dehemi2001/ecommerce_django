a674a8d8fbec9b1d60e930ab8e8991d7cd8ac453
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-06-07 14:44:54 +0530

    Account Activation – Encode User PK & Send Token Based Activation Link

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.tokens import default_token_generator
 from accounts.models import Account
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+
+# Verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -25,6 +34,21 @@ def register(request):
                 )
             user.phone_number = phone_number
             user.save()
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user' : user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            
             messages.success(request, 'Registration successful!')
             return redirect('register')
     else:
--- ecommerce_django/settings.py
+++ ecommerce_django/settings.py
@@ -138,3 +138,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# SMTP CONFIGURATION
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
