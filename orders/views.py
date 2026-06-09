a17c3b4adcfdbfbddb160d36c92fac5bb6ee5e6c
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-06-10 01:09:58 +0530

    Redirect the User to Order Completed Page

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.template import context
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from carts.models import CartItem
 from .forms import OrderForm
@@ -69,8 +69,11 @@ def payments(request):
     send_email.send()
 
     # Send order number and transaction id back to sendData method via JsonResponse
-
-    return render(request, 'orders/payments.html')
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
 
 def place_order(request, total=0, quantity=0,):
     current_user = request.user
@@ -131,3 +134,6 @@ def place_order(request, total=0, quantity=0,):
             return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
+
+def order_complete(request):
+    return render(request, 'orders/order_complete.html')
