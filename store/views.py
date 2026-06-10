2617b0039d6cb326eb3fa9b8ff3e421bfc556c22
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-06-10 12:43:26 +0530

    Check if the User has purchased the Product before submitting Review

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from orders.models import OrderProduct
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from store.models import Product, ReviewRating
@@ -42,9 +43,16 @@ def product_detail(request, category_slug, product_slug):
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
+
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'orderproduct': orderproduct,
     }
     return render(request, 'store/product_detail.html', context)
 
