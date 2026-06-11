f339895500939a2696f5b583d25dbe1c14de5df6
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-06-11 12:33:54 +0530

    Environment variable bugs fixed.

--- ecommerce_django/settings.py
+++ ecommerce_django/settings.py
@@ -26,9 +26,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
