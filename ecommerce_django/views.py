30cf3c88cce8dbc7efe235f10876eb45e68291a4
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-06-12 18:03:55 +0530

    Popular product query changed

--- ecommerce_django/views.py
+++ ecommerce_django/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from store.models import Product, ReviewRating
+from django.db.models import Avg, Q
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('created_date')
-
-    # Get the reviews
-    reviews = None
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+    # Get top 8 products based on average rating of approved reviews
+    products = Product.objects.filter(is_available=True).annotate(
+        avg_rating=Avg('reviewrating__rating', filter=Q(reviewrating__status=True))
+    ).order_by('-avg_rating', '-created_date')[:8]
 
     context = {
         'products': products,
-        'reviews': reviews,
     }
     return render(request, 'home.html', context)
